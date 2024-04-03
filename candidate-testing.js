4326060ce9406dc6254c35cc803ff601d1c69193
loganrains <logankrains@gmail.com>
2024-04-03 12:42:57 -0500

    Completed GA1 Part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,13 +12,13 @@ let candidateAnswer = "";
 
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride","40", "true", "Trajectory", "3"];
+let correctAnswers = ["Sally Ride","true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Hello User, please enter your name below.\n");
+  candidateName = input.question("\nHello User, please enter your name: ");
 
 }
 
@@ -29,6 +29,7 @@ function askQuestion() {
   for (let i = 0; i < questions.length; i++){
     console.log(questions[i])
     candidateAnswers[i] = input.question("Enter answer here: ");
+    console.log("");
   }
 }
 
@@ -40,21 +41,46 @@ function gradeQuiz(candidateAnswers) {
   // } else {
   //   console.log("Incorrect");
   // }
-  console.log(`Your Answer: ${candidateAnswers}\nCorrect Answer: ${correctAnswers}`);
 
+  // console.log(`Your Answer: ${candidateAnswers}\nCorrect Answer: ${correctAnswers}`);
+  let numCorrectAnswers = 0;
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  console.log(`Candidate Name: ${candidateName}`);
+  for (let i = 0; i < questions.length; i++){
+    console.log(`${i + 1}) ${questions[i]}`);
+    console.log(`Your Answer: ${candidateAnswers[i]}`);
+    console.log(`Correct Answer: ${correctAnswers[i]}\n`);
+
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+        numCorrectAnswers++
+    }
+  }
+
+  grade = (numCorrectAnswers/questions.length)*100;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
 }
 
 function runProgram() {
+  
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("\nWelcome, " + candidateName + "\n");
+  // console.log("\nWelcome, " + candidateName + "\n");
+  
+  console.log(`\nWelcome ${candidateName}! Please answer the following questions to the best of your ability.\n`);
+  
   askQuestion();
+  
   gradeQuiz(this.candidateAnswers);
+
+  console.log(`>>> Overall Grade: ${grade}% (${(grade/20)} of ${correctAnswers.length} responses correct) <<<`); 
+  
+  if(grade >= 80){
+    console.log(">>> Status: PASSED <<<");
+  } else {
+    console.log(">>> Status: FAILED <<<");
+  }
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
